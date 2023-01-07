622537ab3ceb56a24718deee578c7f8144df2d7b
Zlata Murmantseva <lyncs.wallet@gmail.com>
2023-01-07 23:05:14 +0300

    Add map

--- map_of_flats/flats_to_map.py
+++ map_of_flats/flats_to_map.py
@@ -10,28 +10,41 @@ geolocator = Nominatim(user_agent="my_request")
 all_pages = ['1-100', '101-200', '201-300', '301-500',
              '501-700', '701-800', '801-1000', '1001-1200', '1201-1300']
 start = 'flats'
-def flats_to_map(pages):
+
+
+def flats_to_map(pages, lst):
 
     map = folium.Map(
         location=[55.753300, 37.624239],  # широта и долгота России
         zoom_start=10
     )
-    start = './flats/flats'
+    start = ''
+    start_new = './flats/new/flats'
+    start_old = './flats/old/flats'
+    if pages[:3] == 'new':
+        start = start_new
+    else:
+        start = start_old
+    pages = pages[4:]
     mCluster = MarkerCluster(name='Marker').add_to(map)
+
     result = start + pages + '.json'
-    with open(result) as f:
-        flats = json.load(f)
+
+    with open(result, 'r', encoding='utf-8') as f:
+        flats = json.loads(f.read())
 
     for flat in flats:
         string = flat['title'] + '\n' + flat['cost'] + '\n' + flat['address'][0]
-        html = string.replace('\n', '<br>').replace('\t', 'nbsp;')
+        popup_t = f'<a href="/mortgage/{flat["cost"]}">{string}</a>'
+        test = folium.Html(popup_t, script=True)
+
 
         folium.Marker(
             location=flat['coordinates'],
-            popup=string,
+            popup=folium.Popup(test, min_width=150, max_width=500),
         ).add_to(mCluster)
 
     map.save(r'map.html')
     #webbrowser.open('map.html')
 
-#flats_to_map('1-100')
+#flats_to_map('new 201-300', [1])
--- map_of_flats/main.py
+++ map_of_flats/main.py
@@ -1,31 +1,104 @@
 # -*- coding: utf-8 -*-
-import dash_leaflet as dl
-from dash import Dash, dcc, html, Input, Output, State
+from dash import Dash, dcc, html, Input, Output
 from flats_to_map import flats_to_map
+import dash_bootstrap_components as dbc
 
-app = Dash()
+from project_python.banki_parser.start_scrapper import get_banks
+
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
+server = app.server
+
+ALLOWED_CHOICE = (
+    "initialFee",
+    "salary(default: 30 000)", "If You have child born after 2018, enter 1, else 0(default: 1)"
+)
+button = html.Div(
+    [
+        dcc.Markdown('''Введите значение: Первоначальный взнос(стандартно: 2 000 000), 
+    Максимальная ежемесячная плата по ипотеке(стандартно: 30 000), Если у вас есть ребенок, рожденный до 2018 года?, введите 1, иначе 0(стандартно: 1)'''),
+        dcc.Input(id="fee", type="number", placeholder="Первоначальный взнос"),
+        dcc.Input(
+            id="max_payment", type="number", placeholder="Максимальная ежемесячная плата по ипотеке",
+        ),
+dcc.Input(
+            id="child", type="number", placeholder="Есть ли у вас ребенок, рожденный до 2018 года?",
+        ),
+        html.Hr(),
+        html.Div(id="number-out"),
+    ],
+    style={'text-align': 'center'}
+)
+
+
 app.layout = html.Div([
     html.H1('Квартиры в Москве и МО'),
-              dcc.RadioItems(
-                id='pages',
-                options=['1-100', '101-200', '201-300',
-                         '301-500', '501-700', '701-800',
-                         '801-1000', '1001-1200', '1201-1300'],
-                value='1-100',
-                inline=True
-            ),
+    html.Div(
+        dcc.RadioItems(
+            id='pages',
+            options=['old 1-100', 'old 101-200', 'old 201-300',
+                     'old 301-500', 'old 501-700', 'old 701-800',
+                     'old 801-1000', 'old 1001-1200', 'old 1201-1300',
+                     'new 1-100', 'new 101-200', 'new 201-300', 'new 301-400',
+                     'new 401-500'],
+            value='old 1-100',
+            inline=True,
+            style={'padding': '5px', 'background-color': '#cdcdcd', 'margin': '3px'}
+        ),
+        style={'width': '90%'}
+    ),
+    button,
     html.Iframe(id='map', srcDoc=open('map.html', 'r', encoding='utf-8').read(), width='100%', height='600')
-])
+],
+style={'text-align': 'center'})
+
+PAYLOAD = {
+    'fee': 2_000_000,
+    'max_payment': 30_000,
+    'child': 1,
+    'type': 'new'
+}
+
+@app.callback(
+    Output("number-out", "children"),
+    Input("fee", "value"),
+    Input("max_payment", "value"),
+    Input("child", "value")
+)
+def number_render(fee, max_payment, child):
+    if fee is not None and int(fee) >= 0:
+        PAYLOAD['fee'] = int(fee)
+    else:
+        fee = str(PAYLOAD['fee'])
+    if fee is not None and int(fee) >= 0:
+        PAYLOAD['max_payment'] = int(max_payment)
+    else:
+        max_payment = str(PAYLOAD['max_payment'])
+    if child == '0' or child == '1':
+        PAYLOAD['child'] = int(child)
+    else:
+        child = str(PAYLOAD['child'])
+    return "Первоначальный взнос: {}, Максимальная ежемесячная плата по ипотеке:" \
+           " {}, Есть ли у вас ребенок, рожденный до 2018 года?: {}" \
+           "".format(fee, max_payment, 'Да' if int(child) else 'Нет')
+
+
 @app.callback(
     Output("map", "srcDoc"),
     Input("pages", "value"))
 def display_choropleth(pages):
-    flats_to_map(str(pages))
+    flats_to_map(str(pages), [1, 1, 1])
+    PAYLOAD['type'] = pages[:3]
 
     fig = open('map.html', 'r', encoding='utf-8').read()
 
     return fig
 
+
+@server.route('/mortgage/<int:price>')
+def mortgage(price):
+    return get_banks(price, PAYLOAD['fee'], PAYLOAD['child'], PAYLOAD['type'], PAYLOAD['max_payment'])
+
+
 if __name__ == '__main__':
     app.run_server()
-
